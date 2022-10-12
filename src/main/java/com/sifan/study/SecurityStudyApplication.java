6d0803ed5cc6f8dda28607e554c2e02bea9de96c
思凡 <2262427313@qq.com>
2022-10-12 13:05:43 +0800

    重新命名描述文件

--- src/main/java/com/sifan/study/SecurityStudyApplication.java
+++ src/main/java/com/sifan/study/SecurityStudyApplication.java
@@ -1,9 +1,12 @@
 package com.sifan.study;
 
+import org.apache.ibatis.annotations.Mapper;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.sifan.study.mapper")
 public class SecurityStudyApplication {
 
     public static void main(String[] args) {
